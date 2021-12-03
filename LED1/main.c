f41b4a2b17d5080c26d8e7025b783fa029d90158
kanekikeh <914493688@qq.com>
2021-12-03 16:44:24 +0800

    uc&RTC

---
+++ LED1/main.c
@@ -0,0 +1,75 @@
+//--------------APB2使能时钟寄存器------------------------
+#define RCC_AP2ENR	*((unsigned volatile int*)0x40021018)
+	//----------------GPIOA配置寄存器 ------------------------
+#define GPIOA_CRL	*((unsigned volatile int*)0x40010800)
+#define	GPIOA_ORD	*((unsigned volatile int*)0x4001080C)
+//----------------GPIOB配置寄存器 ------------------------
+#define GPIOB_CRH	*((unsigned volatile int*)0x40010C04)
+#define	GPIOB_ORD	*((unsigned volatile int*)0x40010C0C)
+//----------------GPIOC配置寄存器 ------------------------
+#define GPIOC_CRH	*((unsigned volatile int*)0x40011004)
+#define	GPIOC_ORD	*((unsigned volatile int*)0x4001100C)
+//-------------------简单的延时函数-----------------------
+void SystemInit(void);
+void Delay_ms(volatile  unsigned  int);
+void A_LED_LIGHT(void);
+void B_LED_LIGHT(void);
+void C_LED_LIGHT(void);
+void  Delay_ms( volatile  unsigned  int  t)
+{
+     unsigned  int  i;
+     while(t--)
+         for (i=0;i<800;i++);
+}
+void A_LED_LIGHT(){
+	GPIOA_ORD=0x0<<0;		//PA0低电平
+	GPIOB_ORD=0x1<<9;		//PB9高电平
+	GPIOC_ORD=0x1<<15;		//PC15高电平
+}
+void B_LED_LIGHT(){
+	GPIOA_ORD=0x1<<0;		//PA0高电平
+	GPIOB_ORD=0x0<<9;		//PB9低电平
+	GPIOC_ORD=0x1<<15;		//PC15高电平
+}
+void C_LED_LIGHT(){
+	GPIOA_ORD=0x1<<0;		//PA0高电平
+	GPIOB_ORD=0x1<<9;		//PB9高电平
+	GPIOC_ORD=0x0<<15;		//PC15低电平	
+}
+//------------------------主函数--------------------------
+int main()
+{
+	int j=100;
+	RCC_AP2ENR|=1<<2;			//APB2-GPIOA外设时钟使能
+	RCC_AP2ENR|=1<<3;			//APB2-GPIOB外设时钟使能	
+	RCC_AP2ENR|=1<<4;			//APB2-GPIOC外设时钟使能
+	//这两行代码可以合为 RCC_APB2ENR|=1<<3|1<<4;
+	GPIOA_CRL&=0xFFFFFFF0;		//设置位 清零	
+	GPIOA_CRL|=0x00000002;		//PA0推挽输出
+	GPIOA_ORD|=1<<0;			//设置PA0初始灯为灭
+	
+	GPIOB_CRH&=0xFFFFFF0F;		//设置位 清零	
+	GPIOB_CRH|=0x00000020;		//PB9推挽输出
+	GPIOB_ORD|=1<<9;			//设置初始灯为灭
+	
+	GPIOC_CRH&=0x0FFFFFFF;		//设置位 清零
+	GPIOC_CRH|=0x30000000;   	//PC15推挽输出
+	GPIOC_ORD|=0x1<<15;			//设置初始灯为灭	
+	while(j)
+	{	
+		A_LED_LIGHT();	
+		Delay_ms(1000000);
+		B_LED_LIGHT();
+		Delay_ms(1000000);
+		C_LED_LIGHT();
+		Delay_ms(1000000);
+	}
+}
+
+
+
+
+void SystemInit(){
+	
+}
+
